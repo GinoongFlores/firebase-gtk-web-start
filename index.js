f74ef22638673eefb14a6306e76973eff2265e57
GinoongFlores <f.christianpaul@outlook.com>
2021-09-02 20:01:00 +0800

    2 files modified

--- index.js
+++ index.js
@@ -50,3 +50,16 @@ async function main() {
   // const ui = new firebaseui.auth.AuthUI(auth);
 }
 main();
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDj4RNohGUdcRR1slvgZdUVNbKJ2odJkNc",
+  authDomain: "fir-web-codelab-398dd.firebaseapp.com",
+  projectId: "fir-web-codelab-398dd",
+  storageBucket: "fir-web-codelab-398dd.appspot.com",
+  messagingSenderId: "390940575886",
+  appId: "1:390940575886:web:5fc4e675e08423b59a21df"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
