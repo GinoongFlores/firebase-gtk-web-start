6ecec984c563e73f4ea9119400dd863918bd4843
GinoongFlores <f.christianpaul@outlook.com>
2021-09-04 20:47:24 +0800

    1 file modified

--- index.js
+++ index.js
@@ -26,10 +26,21 @@ let guestbookListener = null;
 let db, auth;
 
 async function main() {
-  // Add Firebase project configuration object here
-  const firebaseConfig = {};
-
   // initializeApp(firebaseConfig);
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: 'AIzaSyDj4RNohGUdcRR1slvgZdUVNbKJ2odJkNc',
+    authDomain: 'fir-web-codelab-398dd.firebaseapp.com',
+    projectId: 'fir-web-codelab-398dd',
+    storageBucket: 'fir-web-codelab-398dd.appspot.com',
+    messagingSenderId: '390940575886',
+    appId: '1:390940575886:web:5fc4e675e08423b59a21df'
+  };
+  // Initialize Firebase
+  initializeApp(firebaseConfig);
+  auth = getAuth();
+
+  // const app = initializeApp(firebaseConfig);
 
   // FirebaseUI config
   const uiConfig = {
@@ -55,19 +66,3 @@ async function main() {
   });
 }
 main();
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: 'AIzaSyDj4RNohGUdcRR1slvgZdUVNbKJ2odJkNc',
-  authDomain: 'fir-web-codelab-398dd.firebaseapp.com',
-  projectId: 'fir-web-codelab-398dd',
-  storageBucket: 'fir-web-codelab-398dd.appspot.com',
-  messagingSenderId: '390940575886',
-  appId: '1:390940575886:web:5fc4e675e08423b59a21df'
-};
-
-initializeApp(firebaseConfig);
-auth = getAuth();
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
